932f4182273818f7d1564e013b0f1ae8dc42bdb3
agguro <aguasguerreiroroberto@gmail.com>
2016-12-20 13:20:32 +0100

    modified to use function syscall

--- x64/common/current-working-directory/cwd.asm
+++ x64/common/current-working-directory/cwd.asm
@@ -1,72 +1,54 @@
 ; Name:         cwd.asm
-; Build:        see makefile
+; Build:        nasm "-felf64" cwd.asm -l cwd.lst -o cwd.o
+;               ld -s -melf_x86_64 -o cwd cwd.o
 ; Description:  Linux alternative for pwd.
 ;               returns the current working directory.
 
 bits 64
 
 [list -]
-     %include "unistd.inc"
+    %include "unistd.inc"
 [list +]
 
 section .bss
       
 section .data
-     errorNoMemory:            db "Error: out of memory"
-     lf:                       db 10                                           ; linefeed
-     errorNoMemory.length:     equ $-errorNoMemory
+    string:
+        .noMemory:  db "Error: out of memory"
+        .lf:        db 10                       ; linefeed
+        .length:    equ $-.noMemory
       
 section .text
      global _start       
+     
 _start:
-     xor       rdi, rdi                        ; get start of heap
-     mov       rax, SYS_BRK
-     syscall
-     and       rax, rax
-     js        error                           ; no more memory available
-     mov       r8, rax                         ; save the current memory break
-repeat: 
-     add       rax, 16                         ; add 16 bytes to the current memory break
-     mov       rdi, rax
-     mov       rax, SYS_BRK
-     syscall
-     xor       rax, rdi                        ; rax = new memory pointer
-     jz        getcwd                          ; if zero then memory is allocated
+     syscall    brk, 0                          ; get start of heap
+     and        rax, rax                        ; if result is negative then error
+     js         error                           ; no more memory available
+     mov        r8, rax                         ; save the current memory break
+repeat:
+     add        rax, 16                         ; add 16 bytes to the current memory break
+     syscall    brk, rax
+     xor        rax, rdi                        ; rax = new memory pointer, test if different from start of heap
+     jz         getcwd                          ; if zero then memory is allocated
      ; no memory could be allocated, free the memory already allocated
-     mov       rdi, r8
-     mov       rax, SYS_BRK
-     jmp       error
+     syscall    brk, r8
+     jmp        error
 getcwd:
-     mov       rsi, rdi
-     sub       rsi, r8
-     mov       rdi, r8                         ; start of buffer
-     mov       rax, SYS_GETCWD                 ; get current working directory
-     syscall
-     and       rax, rax
-     jns       printit                         ; if RAX < 0 then buffer not large enough
-     mov       rax, rdi                        ; buffer not large enough rax = r8
-     add       rax, rsi                        ; add size of already allocated memory -> new memory break
-     jmp       repeat                          ; retry allocating more memory
+     sub        rdi, r8
+     syscall    getcwd, r8, rdi
+     and        rax, rax
+     jns        printit                         ; if RAX < 0 then buffer not large enough
+     mov        rax, rdi                        ; buffer not large enough rax = r8
+     add        rax, rsi                        ; add size of already allocated memory -> new memory break
+     jmp        repeat                          ; retry allocating more memory
 printit:        
-     mov       rsi, r8                         ; start of heap in rsi
-     mov       rdx, rax                        ; length of directory string in rdx
-     xor       rax, rax
-     inc       rax                             ; rax = 1 : syscall write
-     mov       rdi, rax                        ; rdi = 1 : STDOUT
-     syscall                                   ; write directory to STDOUT
-     mov       rsi, lf                         ; linefeed
-     mov       rdx, rdi                        ; rdx = rdi = 1 : length of linefeed (1 byte)
-     mov       rax, rdi                        ; rax = rdi = 1 : syscall write
-     syscall                                   ; write linefeed to STDOUT
-     jmp       exit
+     syscall    write, stdout, r8, rax          ; write directory to stdout
+     syscall    write, stdout, string.lf, 1     ; write linefeed to stdout
+     jmp        exit
+     
 error:
-     mov       rsi, errorNoMemory              ; rsi : error string
-     mov       rdx, errorNoMemory.length       ; rdx : error string length
-     xor       rax, rax
-     inc       rax                             ; rax = 1 : syscall write
-     mov       rdi, rax                        ; rdi = 1 : STDOUT
-     syscall                                   ; write error string
+
+     syscall    write, stderr, string.noMemory, string.length       ; write error string
 exit:
-     xor       rdi, rdi                        ; no error
-     mov       rax, SYS_EXIT                   ; exit
-     syscall
+     syscall    exit, 0
