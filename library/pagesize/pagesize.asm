29b651141c0447811e785dd92ba6a5df5c4acef3
agguro <admin@agguro.org>
2017-08-09 23:13:42 +0200

    fixed pops at end of file and added check on carry bit set indicating error on r14

--- library/pagesize/pagesize.asm
+++ library/pagesize/pagesize.asm
@@ -16,14 +16,9 @@ bits 64
 global pagesize
     
 section .text
-    global _start
-    
-_start:
-    call    pagesize
-    syscall exit,0
-    
+
 pagesize:
-    push        r15
+    push        r15                         ;save help registers
     push        r14
     push        r10
     push        r9
@@ -44,24 +39,26 @@ pagesize:
     mov         r15,rax
     add         r15,r14
     syscall     munmap,r15,r14
-    mov         r8,rax                     ;save error code
+    mov         r8,rax                      ;save error code
     mov         r15,r14
     shl         r15,1
     syscall     munmap,rdx,r15
     mov         rax,r14
-    and         r8,r8
+    and         r8,r8                       ;if r8==0 then we have our pagesize
     jz          .exit
-    shl         r14,1
-    jmp         .repeat
+    clc                                     ;clear carry bit
+    rcl         r14,1                       ;we do a rotate instead of a shift
+    jc          .failed                     ;to avoid an endless loop
+    jmp         .repeat                     ;still not there
 .failed:
     mov         rax,-1
 .exit:
-    push        rdi
-    push        rsi
-    push        rdx
-    push        r8
-    push        r9
-    push        r10
-    push        r14
-    push        r15
+    pop         rdi                         ;restore used registers
+    pop         rsi
+    pop         rdx
+    pop         r8
+    pop         r9
+    pop         r10
+    pop         r14
+    pop         r15
     ret
