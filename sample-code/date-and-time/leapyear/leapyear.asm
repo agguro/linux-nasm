b87ba176e62162afd455b76e7f596a7d243528c6
agguro <admin@agguro.org>
2017-08-11 17:13:35 +0200

    added comments

--- sample-code/date-and-time/leapyear/leapyear.asm
+++ sample-code/date-and-time/leapyear/leapyear.asm
@@ -11,8 +11,7 @@ bits 64
 [list -]
       %include "unistd.inc"
 [list +]
- 
- 
+
 section .bss
       sign:             resb    1
 section .data
@@ -20,10 +19,15 @@ section .data
       leap:             db      "leapyear", 10
       noleap.length:    equ     $-noleap
       leap.length:      equ     $-leap
-      usage:            db      "usage: leapyear year", 10
-                        db      "       year must be an integer bigger than -46 and not larger than a 18446744073709551615 bits integer and non-zero.", 10
-                        db      "                 not less than -45 because Julian took effect in 45 B.C.",10
-                        db      "                 not zero because zero wasn't a year at all.", 10
+      usage:            db      "usage: ",0x1B,"[1m"
+                        db      "leapyear"
+                        db      0x1B,"[0m"
+                        db      " year", 10
+                        db      "       year must be between -45 and 18446744073709551615 and not 0.",10
+                        db      0x1B,"[3m"
+                        db      "       - not before -45 because Julian took effect in 45 B.C.",10
+                        db      "       - not zero because zero was not a year at all.",10
+                        db      0x1B,"[0m"
       .length:          equ     $-usage
       
 section .text
@@ -31,55 +35,57 @@ section .text
  
 _start:
       xor       r8, r8             ; sign flag = 0 (no minus sign)
-      pop       rax
-      cmp       rax, 1
-      jle       .usage
-      cmp       rax, 2
-      jg        .usage
-      pop       rax
-      pop       rsi
+      pop       rax                ; get argc
+      cmp       rax, 1             ; no arguments
+      je        .usage             ; print usage
+      cmp       rax, 2             ; if more than two arguments
+      jg        .usage             ; print usage
+      pop       rax                ; get name of this program
+      pop       rsi                ; get pointer to year string
       xor       rax, rax
       cld
 .repeat:
-      lodsb
-      and       al, al
+      lodsb                        ; read byte at rsi
+      and       al, al             ; if zero then we are at the end of string
       jz        .done
-      cmp       al, "-"
-      jne        .nosign
-      cmp       r8, 1                ; if sign is already set then we have two signs thus an error
-      je        .usage
-      mov       r8, 1
-      jmp       .repeat
-.nosign:      
+      cmp       al, "-"            ; first character can be minus
+      jne        .nosign           ; if not then it may not appear again
+      cmp       r8, 1              ; if sign is already set then we have two signs
+      je        .usage             ; which is wrong so print usage
+      mov       r8, 1              ; set sign flag r8
+      jmp       .repeat            ; look for other minus signs
+.nosign:                           ; now check rest of string
       cmp       al, "0"
-      jb        .usage
+      jb        .usage             ; if char below '0' then wrong input
       cmp       al, "9"
-      ja        .usage
-      and       al, 0x0F
-      shl       rdx, 1
-      mov       rcx, rdx
-      shl       rdx, 2
-      add       rdx, rcx
-      add       rdx, rax
-      jmp       .repeat
+      ja        .usage             ; if char above '9' then wrong input
+      and       al, 0x0F           ; unascii char
+      ; fast multiply temporary result by ten
+      shl       rdx, 1             ; rdx = rdx * 2
+      mov       rcx, rdx           ; store in rcx = rdx * 2
+      shl       rdx, 2             ; rdx = rdx * 4
+      add       rdx, rcx           ; rdx = rdx * 10
+      add       rdx, rax           ; add digit to rdx
+      jmp       .repeat            ; repeat for all characters in string
 .done:
-      mov       rax, rdx
-      and       rax, rax
-      jz        .usage
-      cmp       r8, 1
-      jne       .calculate
-      cmp       rax, 45
-      jg        .usage
+      mov       rax, rdx           ; move decimal in rax
+      and       rax, rax           ; if zero
+      jz        .usage             ; then print usage, zero wasn't a year
+      cmp       r8, 1              ; was year before 0, signflag in r8 = 1
+      jne       .calculate         ; no, calculate leapyear
+      cmp       rax, 45            ; yes, check the year
+      jg        .usage             ; if greater than 45, print usage
 .calculate:
-      call      LeapYear
-      and       al, al
-      jz        .noleap
-      mov       rsi, leap
+      mov       rdi, rax           ; rdi = year (ABI)
+      call      LeapYear           ; calculate leapyear
+      and       rax, rax           ; if AL = -1 then no leapyear
+      js        .noleap            ; year was no leap year
+      mov       rsi, leap          ; year was a leap year
       mov       rdx, leap.length
       jmp       .write
 .noleap:
       mov       rsi, noleap
-      mov       rdx, noleap.length
+      mov       rdx, noleap.length 
       jmp       .write
 .usage:
       mov       rsi, usage
@@ -90,21 +96,30 @@ _start:
       syscall   exit, 0
 
 ; LeapYear
-; RAX holds the year in hexadecimal format. Year is a positive number.
+; in  :  RDI holds the year in hexadecimal format. Year is a positive number.
+; out :  RAX = -1, no leap, RAX = 0 leap
 LeapYear:
-      xor       rcx, rcx                        ; help register = 0
-      test      rax, 3                          ; last two bits 0?
-      jnz       .@1                             ; if not year is not disible by 4 -> no leapyear
-      inc       rcx                             ; assume year is a leapyear
-      xor       rdx, rdx                        ; prepare rdx for division
-      mov       rbx, 100                        ; year / 100
+      push      rbx                ; save used registers
+      push      rcx
+      push      rdx
+      mov       rax, rdi
+      xor       rcx, rcx           ; help register = 0
+      dec       rcx                ; assume not leap, rcx = -1
+      test      rax, 3             ; last two bits 0?
+      jnz       .@1                ; if not year is not disible by 4 -> no leapyear
+      inc       rcx                ; assume year is a leapyear, rcx = 0
+      xor       rdx, rdx           ; prepare rdx for division
+      mov       rbx, 100           ; year / 100
       div       rbx
-      and       rdx, rdx                        ; remainder = 0?
-      jnz       .@1                             ; no, no leapyear
-      xor       rcx, rcx                        ; yes, multiples of 100 aren't leap years...
-      test      rax, 3                          ; ...except if last two bits are zero 0 (divisble by 4)
-      jnz       .@1                             ; no, not leap year
-      mov       rcx, 1                          ; if last two bits are zero then year is divisible by 4 too, so divisible by 400 = leapyear
+      and       rdx, rdx           ; remainder = 0?
+      jnz       .@1                ; no, no leapyear
+      test      rax, 3             ; multiples of 100 aren't leap years except if last two bits
+                                   ;  are zero 0 (divisible by 4) then also divisible by 400
+      jz        .@1                ; yes, leap year
+      dec       rcx                ; no, not leap year, rcx = -1
 .@1:
-      mov       rax, rcx                        ; mov result in RAX
+      mov       rax, rcx           ; mov result in RAX
+      pop       rdx
+      pop       rcx
+      pop       rbx
       ret
