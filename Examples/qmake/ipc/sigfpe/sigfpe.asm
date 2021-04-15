ec2aa6e1aab939eacb714bd9eb9f7489dee278d0
agguro <aguasguerreiroroberto@gmail.com>
2021-04-15 23:57:34 +0200

    trap division by zero error

--- Examples/qmake/ipc/sigfpe/sigfpe.asm
+++ Examples/qmake/ipc/sigfpe/sigfpe.asm
@@ -1,11 +1,14 @@
 ;name: sigfpe.asm
 ;
-;description:
+;description: Floating point exception signal handler demo
 ;
+;Reference:   Beejs guide to IPC - http://beej.us/guide/bgipc/output/html/multipage/signals.html#catchsig
+;             http://syprog.blogspot.be/2011/10/iterfacing-linux-signals.html for the 64 bit structures
+;             man 7 signal
 
 bits 64
 
-%include "../../../qmake/ipc/sigfpe/sigfpe.inc"
+%include "../sigfpe/sigfpe.inc"
 
 global main
 
@@ -14,19 +17,60 @@ section .bss
 
 section .data
 ;initialized read-write data
+    SIGACTION   sigaction
 
 section .rodata
 ;read-only data
+    ; the messages
+    msgDivisionByZero:  db  "Division by zero error", 10
+    .len:               equ $-msgDivisionByZero
+    ; error message
+    msgSignalError:     db  "sigaction error, terminating program.", 10
+    .len:               equ $-msgSignalError
 
 section .text
 
 main:
-    push    rbp
-    mov     rbp,rsp
-
-    ;TODO: put your code here...
-
+    ;Initialize SIGFPE handler
+    mov     rax,sigFPEHandler                       ;set handler to pointer to procSigInt
+    mov     qword[sigaction.sa_handler],rax         ;in sigaction structure
+    mov     rax,SA_RESTORER | SA_SIGINFO            ;sa_flags
+    mov     qword [sigaction.sa_flags],rax
+    mov     rax,.exit
+    mov     qword[sigaction.sa_restorer],rax
+    mov     r10,NSIG_WORDS                          ;size of mask in 64 bit words
+    syscall rt_sigaction,SIGFPE,sigaction,0
+    ;If rax < 0 , we got an error, rax should be zero
+    and     rax,rax
+    js      .error
+    ;Here we will count down from 10 to 0 each time dividing the counter to itself.
+    ;once zero is reached we should trigger the SIGFPE signal and execute the signal action
+    ;defined in ProcSIGFPE indicating a division by zero occured.
+    ;There we can dump register contents as an example.
+    mov     rcx,10
+.repeat:
+    mov     rax,rcx
+.oper:
+    idiv    rax                                     ;rax=rax/rax
+    ;If no exception occured continue
+    dec     rcx
+    ;Here we check that rcx equals zero and we must exit the loop, however we will not exit and
+    ;demonstrate a wrong compare with zero.
+    and     rcx,rcx
+    jge     .repeat            ;must be jg instead of jge
+.done:
+    jmp     .exit
+    ; in case we have an error
+.error:
+    syscall write, stdout, msgSignalError, msgSignalError.len
+    ; exit the program
+.exit:
     xor     rax,rax             ;return error code
-    mov     rsp,rbp
-    pop     rbp
     ret                         ;exit is handled by compiler
+
+; The SIGFPE Floating point exception handler
+sigFPEHandler:
+    ; rdi=signum, rsi=siginfo_t pointer, rdx=sigcontext*
+    mov     rax,[rdx+UCONTEXT_STRUC.uc_mcontext+SIGCONTEXT_STRUC.rip]       ; get rip where error occured
+    syscall write,stderr,msgDivisionByZero,msgDivisionByZero.len
+    ret
