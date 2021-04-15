31cc0b7ea6815339f17ccdca36a7256c61392560
agguro <aguasguerreiroroberto@gmail.com>
2021-04-15 22:37:58 +0200

    minor update

--- Examples/native/interprocess-communication/exeapp1.asm
+++ Examples/native/interprocess-communication/exeapp1.asm
@@ -1,9 +1,9 @@
-;Name:         exeapp1.asm
+;name:         exeapp1.asm
 ;
-;Build:        nasm "-felf64" exeapp4.asm -l exeapp4.lst -o exeapp4.o
+;build:        nasm "-felf64" exeapp4.asm -l exeapp4.lst -o exeapp4.o
 ;              ld -s -melf_x86_64 -o exeapp4 exeapp4.o
 ;
-;Description:  First demonstration of four examples on how to execute a program or shell script from within an
+;description:  First demonstration of four examples on how to execute a program or shell script from within an
 ;              assembler program. Assumed is that the 'hello' program is present, that you
 ;              know the path to it and the hello program must be executable. (chmod +x hello).
 ;              If everything behaves, the child exits after the execution of the child process (the hello application)
