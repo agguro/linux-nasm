f9c79bc773387de27536e891dfccec64047c24cc
Agguro <aguasguerreiroroberto@gmail.com>
2022-03-26 17:20:56 +0100

    update

--- to.revised/bootable-cdrom/bootcat/bootcat.asm
+++
@@ -1,9 +0,0 @@
-org 0
-bits 16
-
-bootcat:
-     dd   1
-     db   "Phoenix Technologies LTD4"
-     db   0xE4
-     db   0x55, 0xAA                              ; must be last
-times 2048-($-bootcat)   db 0
