2f2b4275de1d2f8effc323648bc2bffb9df3bff7
agguro <aguasguerreiroroberto@gmail.com>
2023-10-20 05:20:48 +0200

    Self modifying code example2 done

--- done/SourceCode/SMC/Example2/smc_example2.asm
+++ done/SourceCode/SMC/Example2/smc_example2.asm
@@ -1,6 +1,7 @@
 ;name: smc_example2.asm
 ;
-;build: 
+;build: /usr/bin/nasm -felf64 -Fdwarf -o smc_example2.o smc_example2.asm
+;       ld -melf_x86_64 -g -o smc_example2 smc_example2.o
 ;
 ;source: https://stackoverflow.com/questions/4812869/how-to-write-self-modifying-code-in-x86-assembly
 ;        [CasseroleBoi]
@@ -23,46 +24,46 @@ exec: resb 4096
 
 section .text
 
-    mprotectsuccess: db "mprotect output value 0", 0x0A
-    .len:   equ $-mprotectsuccess
+    mprotectsuccess: db "mprotect output value 0.", 0x0A
+    .len:            equ $-mprotectsuccess
 
-    mprotectfailure: db "mprotect failure, segfault", 0x0A
-    .len:   equ $-mprotectfailure
+    mprotectfailure: db "mprotect failure.", 0x0A
+    .len:            equ $-mprotectfailure
 
     message:	db "copied code executed!", 0x0A
     .len:	equ	$-message
-    
+
 global _start
 
 _start:
-
 
     syscall mprotect,exec,4096,PROT_EXEC | PROT_READ | PROT_WRITE
     and rax, rax
     jz  .success
 .failure:
+    ;the memory in the .bss section couldn't be protected for whatever
+    ;reason.  Just show that there is a problem without detail.
     syscall write, stdout,mprotectfailure,mprotectfailure.len
-    jmp .continue                   ;run into trouble
+    jmp .continue                           ;run into trouble
 .success:
     syscall write, stdout,mprotectsuccess,mprotectsuccess.len
 .continue:
 
     mov  r15, back
 
-    ;copy code into section .bss   
-    mov	rsi,code
-    mov	rdi,exec
-    mov rcx,code.len
-    rep movsb
+    ;copy code into section .bss
 
-    mov rax, exec
-    jmp rax
-    
-back:   
+    mov	   rsi,code
+    mov	   rdi,exec
+    mov    rcx,code.len
+    rep    movsb
+    mov    rax, exec
+    jmp    rax
+
+back:
     syscall exit,0
 
-    
 code:
-	syscall write,stdout,message,message.len
-	jmp r15
-.len: equ $-code	
+    syscall write,stdout,message,message.len
+    jmp     r15
+.len: equ $-code
