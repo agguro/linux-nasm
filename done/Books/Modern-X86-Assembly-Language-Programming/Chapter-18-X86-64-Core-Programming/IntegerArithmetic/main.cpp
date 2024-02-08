79154be1c299592e37c9c9de46330f746ee094b4
Agguro <aguasguerreiroroberto@gmail.com>
2024-02-08 22:58:20 +0100

    update 20240209-22:58

---
+++ done/Books/Modern-X86-Assembly-Language-Programming/Chapter-18-X86-64-Core-Programming/IntegerArithmetic/main.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "../../CommonFiles/miscdefs.h"
+
+extern "C" Int64 IntegerAdd(Int64 a, Int64 b, Int64 c, Int64 d, Int64 e, Int64 f);
+extern "C" Int64 IntegerMul(Int8 a, Int16 b, Int32 c, Int64 d, Int8 e, Int16 f, Int32 g, Int64 h);
+extern "C" void IntegerDiv(Int64 a, Int64 b, Int64 quo_rem_ab[2], Int64 c, Int64 d, Int64 quo_rem_cd[2]);
+
+void IntegerAddCpp(void)
+{
+    Int64 a = 100;
+    Int64 b = 200;
+    Int64 c = -300;
+    Int64 d = 400;
+    Int64 e = -500;
+    Int64 f = 600;
+
+    // Calculate a + b + c + d + e + f
+    Int64 sum = IntegerAdd(a, b, c, d, e, f);
+
+    printf("\nResults for IntegerAdd\n");
+    printf("a: %" PRId64 " b: %" PRId64 " c: %" PRId64 "\n", a, b, c);
+    printf("d: %" PRId64 " e: %" PRId64 " f: %" PRId64 "\n", d, e, f);
+    printf("sum: %" PRId64 "\n", sum);
+}
+
+void IntegerMulCpp(void)
+{
+    Int8 a = 2;
+    Int16 b = -3;
+    Int32 c = 8;
+    Int64 d = 4;
+    Int8 e = 3;
+    Int16 f = -7;
+    Int32 g = -5;
+    Int64 h = 10;
+
+    // Calculate a * b * c * d * e * f * g * h
+    Int64 result = IntegerMul(a, b, c, d, e, f, g, h);
+
+    printf("\nResults for IntegerMul\n");  
+    printf("a: %" PRId8 " b: %" PRId16 " c: %" PRId32 " d: %" PRId64 "\n", a, b, c, d);
+    printf("a: %" PRId8 " b: %" PRId16 " c: %" PRId32 " d: %" PRId64 "\n", e, f, g, h);
+    printf("result: %" PRId64 "\n", result);
+}
+
+void IntegerDivCpp(void)
+{
+    Int64 a = 102;
+    Int64 b = 7;
+    Int64 quo_rem_ab[2];
+    Int64 c = 61;
+    Int64 d = 9;
+    Int64 quo_rem_cd[2];
+
+    // Calculate a / b  and c / d
+    IntegerDiv(a, b, quo_rem_ab, c, d, quo_rem_cd);
+
+    printf("\nResults for IntegerDiv\n");
+    printf("a:   %" PRId64 " b:   %" PRId64 " ", a, b);
+    printf("quo: %" PRId64 " rem: %" PRId64 "\n", quo_rem_ab[0], quo_rem_ab[1]);
+    printf("c:   %" PRId64 " d:   %" PRId64 " ", c, d);
+    printf("quo: %" PRId64 " rem: %" PRId64 "\n", quo_rem_cd[0], quo_rem_cd[1]);
+}
+
+int main(int argc, char* argv[])
+{
+    IntegerAddCpp();
+    IntegerMulCpp();
+    IntegerDivCpp();
+    return 0;
+}
---
+++ done/Books/Modern-X86-Assembly-Language-Programming/Chapter-18-X86-64-Core-Programming/MemoryAddressing/memoryaddressing.asm
@@ -0,0 +1,75 @@
+; Name:         memoryaddressing.asm
+;
+; Build:        g++ -c main.cpp -o main.o
+;               nasm -f elf64 -o memoryaddressing.o memoryaddressing.asm
+;               g++ -o memoryaddressing memoryaddressing.o main.o
+;
+; Source:       Modern x86 Assembly Language Programming p.512
+
+bits 64
+
+global MemoryAddressing
+global FibVals
+global NumFibVals
+
+section .bss
+    ;value to demo RIP-relative addressing
+    common FibValsSum 4         ;one dword to store result
+                                ;memory is global and shared amongst other modules
+
+section .data
+
+; Simple lookup table
+FibVals:    dd  0,  1,  1,   2,   3,   5,   8,  13,   21
+            dd 34, 55, 89, 144, 233, 377, 610, 987, 1597
+NumFibVals: dd ($ - FibVals) / 4    ; sizeof dword = 4 bytes
+
+section .text
+
+; extern "C" int MemoryAddressing(int i, int* v1, int* v2, int* v3, int* v4);
+;
+; Description:  This function demonstrates various addressing modes
+;               that can be used to access operands in memory.
+;
+; Returns:      0 = error (invalid table index)
+;               1 = success
+
+MemoryAddressing:
+    ; Make sure 'i' is valid
+    cmp     edi,0
+    jl      .invalidIndex               ;jump if i < 0
+    cmp     edi,[NumFibVals]
+    jge     .invalidIndex               ;jump if i >= NumFibVals_
+
+    ; Sign extend i for use in address calculations
+    movsxd  rdi,edi                     ;sign extend i
+    mov     r9,rdi                      ;save copy of i
+    ; Example #1 - base register
+    mov     r11,FibVals                 ;r11 = FibVals
+    shl     rdi,2                       ;rdi = i * 4
+    add     r11,rdi                     ;r11 = FibVals + i * 4
+    mov     eax,[r11]                   ;eax = FibVals[i]
+    mov     [rsi],eax                   ;Save to v1
+    ; Example #2 - base register + index register
+    mov     r11,FibVals                 ;r11 = FibVals
+    mov     rdi,r9                      ;rdi = i
+    shl     rdi,2                       ;rdi = i * 4
+    mov     eax,[r11+rdi]               ;eax = FibVals[i]
+    mov     [rdx],eax                   ;Save to v2
+    ; Example #3 - base register + index register * scale factor
+    mov     r11,FibVals                 ;r11 = FibVals
+    mov     rdi,r9                      ;rdi = i
+    mov     eax,[r11+rdi*4]             ;eax = FibVals[i]
+    mov     [rcx],eax                   ;Save to v3
+    ; Example #4 - base register + index register * scale factor + disp
+    mov     r11,FibVals-42              ;r11 = FibVals - 42
+    mov     rdi,r9                      ;rdi = i
+    mov     eax,[r11+rdi*4+42]          ;eax = FibVals[i]
+    mov     [r8],eax                    ;Save to v4
+    ; Example #5 - RIP relative
+    add     [FibValsSum],eax            ;Update sum
+    mov     eax,1                       ;set success return code
+    ret
+.invalidIndex:
+    xor     eax,eax                     ;set error return code
+    ret
