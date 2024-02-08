1f3454f651810cbefe1c27622dea791bf145fe14
Agguro <aguasguerreiroroberto@gmail.com>
2024-02-08 23:00:15 +0100

    update 20240208-23:00

--- Support/make_directories.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-
-find . -name '*.asm' | xargs -I{} basename {} '.asm' | xargs -i mkdir -p {}
-find . -type d | xargs -i cp ./index.php {}
-find . -type d | xargs -i cp ./Makefile {}
