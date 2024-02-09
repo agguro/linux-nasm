48cb190ef718e5c656b5b800fc0692b5e8a53755
Agguro <aguasguerreiroroberto@gmail.com>
2024-02-09 05:16:31 +0100

    typo

--- done/Books/Beginning-X64-Assembly-Programming/Chapter-01/hello.asm
+++ done/Books/Beginning-X64-Assembly-Programming/Chapter-01/hello.asm
@@ -1,7 +1,7 @@
 ; hello.asm
 
 section .data					
-    msg	db      "hello, world",10			
+    msg	db      "hello, world",0		
 
 section .bss							
 
@@ -12,7 +12,7 @@ main:
 	mov     rax, 1		; 1 = write	
     mov     rdi, 1		; 1 = to stdout		
     mov     rsi, msg	; string to display in rsi
-    mov     rdx, 13		; length of the string, without 0
+    mov     rdx, 12		; length of the string, without 0
     syscall				; display the string
     mov     rax, 60		; 60 = exit	
     mov     rdi, 0		; 0 = success exit code	
